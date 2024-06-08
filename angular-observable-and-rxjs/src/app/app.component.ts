cae4a809bbbc47807337f261b8dc03ff0b29bca5
jain-18 <shubhamjain12360@gmail.com>
2024-06-08 14:00:42 +0530

    learned next,error,complete of observable

--- angular-observable-and-rxjs/src/app/app.component.ts
+++ angular-observable-and-rxjs/src/app/app.component.ts
@@ -16,18 +16,38 @@ export class AppComponent{
   //observalble
   myObservable = new  Observable((observer) => {
     setTimeout(() => {observer.next(1)},1000)
-    setTimeout(() => {observer.next(2)},3000)
-    setTimeout(() => {observer.next(3)},5000)
-    setTimeout(() => {observer.next(4)},7000)
-    setTimeout(() => {observer.next(5)},9000)
+    setTimeout(() => {observer.next(2)},2000)
+    setTimeout(() => {observer.error(new Error('something went wrong . Please try again'))},3000)
+    setTimeout(() => {observer.next(3)},3000)
+    setTimeout(() => {observer.next(4)},4000)
+    setTimeout(() => {observer.next(5)},5000)
+    // setTimeout(() => {observer.complete()},6000)
   })
 
 
   GetAsyncData(){
       //observer
-      // next,eeror,complete
-    this.myObservable.subscribe((val:any)=>{
-      this.data.push(val);
-    });
+      // next,error,complete
+  //   this.myObservable.subscribe((val:any)=>{
+  //     this.data.push(val);
+  //   },
+  // (erro)=>{
+  //   alert(erro.message)
+  // },
+  // ()=>{
+  //   alert('All the data is streamed')
+  // });
+    this.myObservable.subscribe({
+      next : (val:any)=> {
+        this.data.push(val);
+      },
+      error(err){
+        alert(err.message);
+      },
+      complete(){
+        alert('All the data stream')
+      }
+    })
+
   }
 }
