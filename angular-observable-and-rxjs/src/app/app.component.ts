12a38c7ba19a8d9633444e6a5505a629317eb781
jain-18 <shubhamjain12360@gmail.com>
2024-06-08 14:25:55 +0530

    learned of and from operator

--- angular-observable-and-rxjs/src/app/app.component.ts
+++ angular-observable-and-rxjs/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +10,33 @@ export class AppComponent{
   title = 'angular-observables';
 
   data: any[] = [];
+  array1 = [1,3,5,7,0];
+  array2 = ['A','B','C','D'];
 
   //1. Create an observable
 
   //observalble
-  myObservable = new  Observable((observer) => {
-    setTimeout(() => {observer.next(1)},1000)
-    setTimeout(() => {observer.next(2)},2000)
-    setTimeout(() => {observer.error(new Error('something went wrong . Please try again'))},3000)
-    setTimeout(() => {observer.next(3)},3000)
-    setTimeout(() => {observer.next(4)},4000)
-    setTimeout(() => {observer.next(5)},5000)
-    // setTimeout(() => {observer.complete()},6000)
+  // myObservable = new  Observable((observer) => {
+  //   setTimeout(() => {observer.next(1)},1000)
+  //   setTimeout(() => {observer.next(2)},2000)
+  //   setTimeout(() => {observer.error(new Error('something went wrong . Please try again'))},3000)
+  //   setTimeout(() => {observer.next(3)},3000)
+  //   setTimeout(() => {observer.next(4)},4000)
+  //   setTimeout(() => {observer.next(5)},5000)
+  //   // setTimeout(() => {observer.complete()},6000)
+  // })
+
+  // of operator
+  // myObservable = of(this.array1,this.array2,20,30,true);
+
+
+  promiseData = new Promise((resolve,reject)=>{
+    resolve([10,20,30,40,50]);
   })
 
+  // from operator
+  // myobservable = from(this.array1);
+  myobservable = from(this.promiseData);
 
   GetAsyncData(){
       //observer
@@ -37,9 +50,10 @@ export class AppComponent{
   // ()=>{
   //   alert('All the data is streamed')
   // });
-    this.myObservable.subscribe({
+    this.myobservable.subscribe({
       next : (val:any)=> {
-        this.data.push(val);
+        this.data.push(val)
+        console.log(this.data);
       },
       error(err){
         alert(err.message);
