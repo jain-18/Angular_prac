efb59f0e0bfa115957999e33369b7eba1ac6dda1
jain-18 <shubhamjain12360@gmail.com>
2024-06-09 18:17:32 +0530

    learned about subject and observable

--- angular-observable-and-rxjs/src/app/subject/subject.component.ts
+++ angular-observable-and-rxjs/src/app/subject/subject.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-subject',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./subject.component.css']
 })
 export class SubjectComponent{
+  ngOnInit(){
+    // let obs  = new Observable((observer)=> {
+      // observer.next(Math.random())
+    // })
+
+    // const subject = new Subject();
+    // // subject.next(Math.random());
+
+    // //Subscriber 1
+    // subject.subscribe((data)=> {
+    //   console.log(data)
+    // })
+    // //subcriber 2
+    // subject.subscribe((data)=> {
+    //   console.log(data)
+    // })
+    // subject.next(Math.random());
+
+    //AJAX
+    const subject = new Subject();
+    const data = ajax('https://randomuser.me/api/')
+
+    subject.subscribe((response)=>console.log(response))
+    subject.subscribe((response)=>console.log(response))
+    subject.subscribe((response)=>console.log(response))
+
+    data.subscribe(subject);
+
+
+  }
 
 }
