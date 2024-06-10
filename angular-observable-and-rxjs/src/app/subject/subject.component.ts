cb1a7f5fd850879e8adfd37b256ffa2cc726306e
jain-18 <shubhamjain12360@gmail.com>
2024-06-10 16:51:25 +0530

    learned behavioural subject

--- angular-observable-and-rxjs/src/app/subject/subject.component.ts
+++ angular-observable-and-rxjs/src/app/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -16,25 +16,35 @@ export class SubjectComponent{
     // const subject = new Subject();
     // // subject.next(Math.random());
 
-    // //Subscriber 1
-    // subject.subscribe((data)=> {
-    //   console.log(data)
-    // })
-    // //subcriber 2
-    // subject.subscribe((data)=> {
-    //   console.log(data)
-    // })
-    // subject.next(Math.random());
+    const subject = new BehaviorSubject<number>(100);
+
+    //Subscriber 1
+    subject.subscribe((data)=> {
+      console.log("1 "+ data)
+    })
+    //subcriber 2
+    subject.subscribe((data)=> {
+      console.log("2 "+data)
+    })
+
+    subject.next(2020);
+
+    //subscribe 3
+    subject.subscribe((data)=> {
+      console.log("3 "+data)
+    })
+
+    subject.next(2023);
 
     //AJAX
-    const subject = new Subject();
-    const data = ajax('https://randomuser.me/api/')
+    // const subject = new Subject();
+    // const data = ajax('https://randomuser.me/api/')
 
-    subject.subscribe((response)=>console.log(response))
-    subject.subscribe((response)=>console.log(response))
-    subject.subscribe((response)=>console.log(response))
+    // subject.subscribe((response)=>console.log(response))
+    // subject.subscribe((response)=>console.log(response))
+    // subject.subscribe((response)=>console.log(response))
 
-    data.subscribe(subject);
+    // data.subscribe(subject);
 
 
   }
