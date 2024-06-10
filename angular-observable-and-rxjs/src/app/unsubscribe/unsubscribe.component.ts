402f6c1684bc1ee8d2c987033b082df1e1b9cc8b
jain-18 <shubhamjain12360@gmail.com>
2024-06-10 17:23:26 +0530

    learned unsubscribe method

--- angular-observable-and-rxjs/src/app/unsubscribe/unsubscribe.component.ts
+++ angular-observable-and-rxjs/src/app/unsubscribe/unsubscribe.component.ts
@@ -7,5 +7,18 @@ import { interval } from 'rxjs';
   styleUrls: ['./unsubscribe.component.css']
 })
 export class UnsubscribeComponent {
-  
+  counter = interval(1000);
+  data:number[] = [];
+  subscriber1;
+
+  OnSubscribe(){
+    this.subscriber1 = this.counter.subscribe((val)=>{
+    this.data.push(val);
+    })
+  }
+
+  OnUnsubcribe(){
+    this.subscriber1.unsubscribe();
+  }
+
 }
