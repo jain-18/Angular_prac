8d0fb142a6a5d8658d4e1e6da3fd8ec2ea56075f
jain-18 <shubhamjain12360@gmail.com>
2024-06-11 10:33:24 +0530

    learned route parameter

--- angular-routing/src/app/courses/course-detail/course-detail.component.ts
+++ angular-routing/src/app/courses/course-detail/course-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/Models/course';
+import { CourseService } from 'src/app/Services/course.service';
 
 @Component({
   selector: 'app-course-detail',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-detail.component.css']
 })
 export class CourseDetailComponent{
+  selectedCourse : Course;
+  courseId : number;
 
+  courseService : CourseService = inject(CourseService);
+  activeRoute : ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(){
+    // this.courseId = this.activeRoute.snapshot.params['id'];
+    this.courseId = +this.activeRoute.snapshot.paramMap.get('id');
+    // console.log(this.courseId);
+    this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+  }
 }
