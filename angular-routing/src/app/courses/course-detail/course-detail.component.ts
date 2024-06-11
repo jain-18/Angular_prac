2446b8b4ba335e5cb20a624a0a58e72827435315
jain-18 <shubhamjain12360@gmail.com>
2024-06-11 11:49:23 +0530

    learned route parameter efficient way

--- angular-routing/src/app/courses/course-detail/course-detail.component.ts
+++ angular-routing/src/app/courses/course-detail/course-detail.component.ts
@@ -14,11 +14,24 @@ export class CourseDetailComponent{
 
   courseService : CourseService = inject(CourseService);
   activeRoute : ActivatedRoute = inject(ActivatedRoute);
+  paramMapObs;
 
   ngOnInit(){
     // this.courseId = this.activeRoute.snapshot.params['id'];
-    this.courseId = +this.activeRoute.snapshot.paramMap.get('id');
+    // this.courseId = +this.activeRoute.snapshot.paramMap.get('id');
     // console.log(this.courseId);
-    this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+    // this.activeRoute.params.subscribe((data)=> {
+    //   this.courseId = +data['id'];
+    //   this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+    // })
+
+    this.paramMapObs = this.activeRoute.paramMap.subscribe((data)=> {
+      this.courseId = +data.get('id');
+      this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+    })
+  }
+
+  ngOnDestroy(){
+    this.paramMapObs.unsubscribe();
   }
 }
