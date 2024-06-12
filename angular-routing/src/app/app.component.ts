6d5433b97a416a813dd20ae5cac90c599b1890a8
jain-18 <shubhamjain12360@gmail.com>
2024-06-12 18:12:07 +0530

    learend to loading page

--- angular-routing/src/app/app.component.ts
+++ angular-routing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { NavigationStart, Router , Event, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 
 @Component({
@@ -8,4 +9,19 @@ import { Component, inject } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Routing';
+  showLoader : boolean = false;
+
+  router : Router = inject(Router);
+
+  ngOnInit(){
+    this.router.events.subscribe((routerEvent : Event)=>{
+      if(routerEvent instanceof NavigationStart){
+        this.showLoader = true;
+      }
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel
+        || routerEvent instanceof NavigationError){
+        this.showLoader=false;
+      }
+    })
+  }
 }
