615040e5e961b2d7b0bf1af22747e83b1b1c5cd4
jain-18 <shubhamjain12360@gmail.com>
2024-06-13 13:36:55 +0530

    learned asyn load

--- angular-pipes/src/app/admin/admin.component.ts
+++ angular-pipes/src/app/admin/admin.component.ts
@@ -17,6 +17,12 @@ export class AdminComponent implements OnInit {
   totalMarks: number;
 
   filterText : string = 'All';
+
+  totalStudents = new Promise((resolve,rejection)=>{
+    setTimeout(()=>{
+      resolve(this.students.length);
+    },2000);
+  })
   
   //PROPERTIES FOR INSERTING
   @ViewChild('name') Name: ElementRef;
