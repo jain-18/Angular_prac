bfdc44fed1a1a1b27a4a0afee111826068bbbcd6
jain-18 <shubhamjain12360@gmail.com>
2024-06-15 10:06:53 +0530

    used ngModel for extracting data

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -11,7 +11,18 @@ export class AppComponent {
 
   @ViewChild('registrationForm') form  : NgForm
 
+  genders = [
+    {id : 'check-male', value : 'male',display : 'Male'},
+    {id : 'check-female', value : 'female', display : 'Female'},
+    {id : 'check-other', value : 'other', display : 'prefered not to say'}
+  ]
+
   OnFormSubmitted(){
-    console.log(this.form)
+    console.log(this.form);
+    console.log(this.form.value.firstname);
+    console.log(this.form.value.lastname);
+    console.log(this.form.value.email);
+    console.log(this.form.value.country);
+    
   }
 }
