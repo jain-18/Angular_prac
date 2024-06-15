9450015da51bb9c5eedb03c7c3dca068c49d4a51
jain-18 <shubhamjain12360@gmail.com>
2024-06-15 12:44:53 +0530

    setValue() adn patchValue()

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   firstName : string ='';
   lastName : string ='';
+  dob : string = '';
   email : string ='';
   
 
@@ -27,7 +28,56 @@ export class AppComponent {
     console.log(this.form.value.firstname);
     console.log(this.form.value.lastname);
     console.log(this.form.value.email);
-    console.log(this.form.value.country);
-    
+    console.log(this.form.value.address.country);
+    console.log(this.form.value.address.city);
   }
+
+  GenerateUsername(){
+    let username = '';
+    if(this.firstName.length >= 3){
+      username += this.firstName.slice(0,3);
+    }
+    else{
+      username += this.firstName;
+    }
+
+    if(this.lastName.length >= 3){
+      username += this.lastName.slice(0,3);
+    }
+    else{
+      username += this.lastName;
+    }
+
+    let datetime = new Date(this.dob) ;
+    username += datetime.getFullYear();
+
+    username = username.toLowerCase();
+
+    console.log(username);
+
+    // this.form.control['username'].value = username;
+    // this.form.setValue({
+    //   firstname : this.form.value.firstname,
+    //   lastname : this.form.value.lastname,
+    //   email : this.form.value.email,
+    //   phone : this.form.value.phone,
+    //   gender : this.form.value.gender,
+    //   dob : this.form.value.dob,
+    //   username : username,
+    //   address: {
+    //     street1 : this.form.value.address.street1,
+    //     street2 : this.form.value.address.street2,
+    //     city : this.form.value.address.city,
+    //     country : this.form.value.address.country,
+    //     region : this.form.value.address.region,
+    //     postal : this.form.value.address.postal
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      username:username
+    })
+  }
+
+
 }
