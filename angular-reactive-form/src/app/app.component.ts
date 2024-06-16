869548bc41ad1193a0686f94b3368a9dec1d9930
jain-18 <shubhamjain12360@gmail.com>
2024-06-16 17:28:05 +0530

    learning reactive form added validation

--- angular-reactive-form/src/app/app.component.ts
+++ angular-reactive-form/src/app/app.component.ts
@@ -1,35 +1,43 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'template-driven-form';
 
-  reactiveForm : FormGroup;
+  reactiveForm: FormGroup;
 
-  ngOnInit(){
-      this.reactiveForm = new FormGroup({
-        firstname :  new FormControl(null,Validators.required),
-        lastname :  new FormControl(null,Validators.required),
-        email :  new FormControl(null,[Validators.required,Validators.email]),
-        username :  new FormControl(null),
-        dob :  new FormControl(null),
-        gender :  new FormControl('male'),
-        country :  new FormControl('India'),
-        city :  new FormControl(null),
-        region :  new FormControl(null),
-        street :  new FormControl(null),
-        postal :  new FormControl(null),
-      })
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null, [Validators.required,Validators.maxLength(10)]),
+      lastname: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      username: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('male'),
+      address: new FormGroup({
+        country: new FormControl('India'),
+        city: new FormControl(null,Validators.required),
+        region: new FormControl(null),
+        street: new FormControl(null,Validators.required),
+        postal: new FormControl(null,Validators.required),
+      }),
+      skills : new FormArray([
+        new FormControl(null, Validators.required),
+        new FormControl(null),
+        new FormControl(null),
+        new FormControl(null)  
+      ])
+    });
   }
 
-  OnFormSubmitted(){
+  OnFormSubmitted() {
     console.log(this.reactiveForm);
   }
-} 
+}
