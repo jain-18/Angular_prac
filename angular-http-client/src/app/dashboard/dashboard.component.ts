6f88401bf1c8cbca17753c08beef1ffdec5aa92b
jain-18 <shubhamjain12360@gmail.com>
2024-06-18 10:18:44 +0530

    learned to post and get

--- angular-http-client/src/app/dashboard/dashboard.component.ts
+++ angular-http-client/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Task } from '../Model/task';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class DashboardComponent{
   showCreateTaskForm: boolean = false;
   http : HttpClient = inject(HttpClient);
+
+  ngOnInit(){
+    this.fetchAllTasks();
+  }
 
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
@@ -26,5 +31,27 @@ export class DashboardComponent{
     subscribe((response)=>{
       console.log(response);
     });
+  }
+
+  // {
+  //   key : {},
+  //   key : {}
+  // }
+
+  private fetchAllTasks(){
+    this.http.get<{[key : string] : Task}>('https://angularprac-89950-default-rtdb.firebaseio.com/tasks.json')
+    .pipe(map((response)=>{
+      //transform data to array
+      let tasks = [];
+      for(let key in response){
+        if(response.hasOwnProperty(key)){
+          tasks.push({id : key , ...response[key]})
+        }
+      }
+      return tasks;
+    }))
+    .subscribe((response)=>{
+      console.log(response);
+    })
   }
 }
