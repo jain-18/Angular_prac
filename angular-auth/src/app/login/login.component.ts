f2017db747daaeefede0dcf84148fea99a803bc0
jain-18 <shubhamjain12360@gmail.com>
2024-06-21 10:41:53 +0530

    learning auth

---
+++ angular-auth/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  isLoginMode : boolean = true;
+
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onFormSubmitted(form : NgForm){
+    console.log(form.value);
+    form.reset();
+
+  }
+}
